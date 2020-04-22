8961c540740c89fd485064f200a50fad385b0e36
Nyíri Péter <nyiprekettye@gmail.com>
2020-04-22 07:47:27 +0200

    test

--- updater-wp-plugin-master-test.php
+++ updater-wp-plugin-master-test.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 /*
 	Plugin Name: updater-wp-plugin-master-test updated version
 	Description: This is for updating your Wordpress plugin.
@@ -6,14 +6,22 @@
 	Author: Nyíri Péter
 	Author URI: 
 */
-if( ! class_exists( 'Smashing_Updater' ) ){
-	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+// if( ! class_exists( 'Smashing_Updater' ) ){
+// 	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+// }
+
+// $updater = new Smashing_Updater( __FILE__ );
+// $updater->set_username( 'nyiprekettye' );
+// $updater->set_repository( 'updater-wp-plugin-master-test' );
+// /*
+// 	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+// */
+// $updater->initialize(); -->
+
+
+require_once( 'BFIGitHubPluginUploader.php' );
+if ( is_admin() ) {
+    new BFIGitHubPluginUpdater( __FILE__, 'nyiprekettye', "updater-wp-plugin-master-test" );
 }
 
-$updater = new Smashing_Updater( __FILE__ );
-$updater->set_username( 'nyiprekettye' );
-$updater->set_repository( 'updater-wp-plugin-master-test' );
-/*
-	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
-*/
-$updater->initialize();
+?>
