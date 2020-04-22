f8b1a23cee3f8e6766e7936cd87ec35e8fb445e4
Nyíri Péter <nyiprekettye@gmail.com>
2020-04-22 09:04:41 +0200

    1.0.6

--- updater-wp-plugin-master-test.php
+++ updater-wp-plugin-master-test.php
@@ -2,27 +2,27 @@
 /*
 	Plugin Name: updater-wp-plugin-master-test updated version
 	Description: This is for updating your Wordpress plugin.
-	Version: 1.0.2
+	Version: 1.0.6
 	Author: Nyíri Péter
 	Author URI: 
 */
-// if( ! class_exists( 'Smashing_Updater' ) ){
-// 	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
-// }
-
-// $updater = new Smashing_Updater( __FILE__ );
-// $updater->set_username( 'nyiprekettye' );
-// $updater->set_repository( 'updater-wp-plugin-master-test' );
-// /*
-// 	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
-// */
-// $updater->initialize(); -->
-
-
-// require_once( 'BFIGitHubPluginUpdater.php' );
-include_once( plugin_dir_path( __FILE__ ) . 'BFIGitHubPluginUpdater.php' );
-if ( is_admin() ) {
-    new BFIGitHubPluginUpdater( __FILE__, 'nyiprekettye', "updater-wp-plugin-master-test" );
+if( ! class_exists( 'Smashing_Updater' ) ){
+	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
 }
 
-?>
+$updater = new Smashing_Updater( __FILE__ );
+$updater->set_username( 'nyiprekettye' );
+$updater->set_repository( 'updater-wp-plugin-master-test' );
+/*
+	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+*/
+$updater->initialize();
+
+
+// // require_once( 'BFIGitHubPluginUpdater.php' );
+// include_once( plugin_dir_path( __FILE__ ) . 'BFIGitHubPluginUpdater.php' );
+// if ( is_admin() ) {
+//     new BFIGitHubPluginUpdater( __FILE__, 'nyiprekettye', "updater-wp-plugin-master-test" );
+// }
+
+// ?>
