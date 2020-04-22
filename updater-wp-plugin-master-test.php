6949ba033cf1882039376f1374071e8cd8a07004
Nyíri Péter <nyiprekettye@gmail.com>
2020-04-22 09:18:08 +0200

    1.0.2

--- updater-wp-plugin-master-test.php
+++ updater-wp-plugin-master-test.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: updater-wp-plugin-master-test updated version
 	Description: This is for updating your Wordpress plugin.
-	Version: 1.0.7
+	Version: 1.0.2
 	Author: Nyíri Péter
 	Author URI: 
 */
