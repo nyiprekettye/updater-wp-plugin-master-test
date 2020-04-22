dbfbd0d6862090d33156b38754f4b06581d4c187
Nyíri Péter <nyiprekettye@gmail.com>
2020-04-22 06:50:59 +0200

    set v 1.0.2

--- updater-wp-plugin-master-test.php
+++ updater-wp-plugin-master-test.php
@@ -1,18 +1,18 @@
 <?php
 /*
-	Plugin Name: Smashing Plugin
+	Plugin Name: updater-wp-plugin-master-test
 	Description: This is for updating your Wordpress plugin.
-	Version: 1.0.0
-	Author: Matthew Ray
-	Author URI: http://www.matthewray.com
+	Version: 1.0.2
+	Author: Nyíri Péter
+	Author URI: 
 */
 if( ! class_exists( 'Smashing_Updater' ) ){
 	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
 }
 
 $updater = new Smashing_Updater( __FILE__ );
-$updater->set_username( 'rayman813' );
-$updater->set_repository( 'smashing-updater-plugin' );
+$updater->set_username( 'nyiprekettye' );
+$updater->set_repository( 'updater-wp-plugin-master-test' );
 /*
 	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
 */
